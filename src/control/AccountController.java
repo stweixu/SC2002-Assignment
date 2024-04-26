ecaf09f8f6e5dda70d385236409b4c004eb65b60
SQ-T30 <128040899+SQ-T30@users.noreply.github.com>
2024-04-26 23:05:22 +0800

    Update AccountController.java

--- src/control/AccountController.java
+++ src/control/AccountController.java
@@ -7,15 +7,18 @@ import exception.StaffNotExistException;
 import exception.WrongPasswordException;
 
 public class AccountController {
-	
+
+	// Check if user account exists in the system
 	public static User checkExisting(String loginID) throws StaffNotExistException {
-		
+
+		// Look for occurrence of loginID among the pool of admin in the company
 		for (User user: Company.getAdmin()) {
 			if (user.getUserId().equals(loginID)) {
 				return user;
 			}
 		}
 		
+		// Look for occurrence of loginID among the pool of staffs in the company
 		for (Branch branch: Company.getBranch().values()) {
 			for (User user: branch.getStaffList()) {
 				if (user.getUserId().equals(loginID)) {
@@ -28,8 +31,9 @@ public class AccountController {
 		throw new StaffNotExistException();
 	}
 
+	// Verify if the password is valid
     public static boolean checkValid(User user, String password) throws WrongPasswordException{
-        if (user.getPassword().equals(password)) 
+        if (user.getPassword().equals(password)) 	
         	return true;
         
         throw new WrongPasswordException();
