6a378725c2f346219c3269b9d54e216bbe545ed9
SQ-T30 <128040899+SQ-T30@users.noreply.github.com>
2024-04-26 23:54:50 +0800

    Update OrderCheckOutInterface.java

--- src/control/customer/OrderCheckOutInterface.java
+++ src/control/customer/OrderCheckOutInterface.java
@@ -1,5 +1,13 @@
 package control.customer;
 
 public interface OrderCheckOutInterface {
+
+	/**
+     * Interface for customer to checkout order
+     * @param branchName The branch to checkout the order in String
+     * @param id The orderID to checkout the order in String
+     * @param send Specifies to checkout as READY_TO_PICKUP or CANCELLED
+     * @return void
+     */
 	public void checkOut(String branchName, String id, boolean send);
 }
