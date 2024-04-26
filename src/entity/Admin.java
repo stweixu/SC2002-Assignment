d5c27fffeff796a37ff8a7a9effd0b0ef8fbb2b0
Khoa <50334353+tranvietkhoa@users.noreply.github.com>
2024-04-26 23:27:57 +0800

    update

--- src/entity/Admin.java
+++ src/entity/Admin.java
@@ -1,7 +1,14 @@
 package entity;
 
 public class Admin extends User{
-	
+	/**
+     * Admin contructor 
+     * @param userName Name of Admin.
+     * @param userId ID of Admin.
+     * @param age age of Admin.
+     * @param gender gender of Admin.
+     * @return void.
+     */
 	public Admin(String userName, String userId, int age, String gender) {
         super(userName, userId, age, gender, Role.ADMIN);
     }
