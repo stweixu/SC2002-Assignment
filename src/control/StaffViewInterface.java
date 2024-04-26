3c74b7dfe9ebaafcbf91d0fb132ed70af3581e54
SQ-T30 <128040899+SQ-T30@users.noreply.github.com>
2024-04-26 23:43:53 +0800

    Update StaffViewInterface.java

--- src/control/StaffViewInterface.java
+++ src/control/StaffViewInterface.java
@@ -1,6 +1,12 @@
 package control;
 
 public interface StaffViewInterface {
+ /**
+     * An interface for Staff to view new orders to be processed under his/her branch
+     * @param branchName The specific branch where the order came from in String.
+     * @return void.
+     */
+  
   public void viewNewOrders(String branchName);
 }
 
