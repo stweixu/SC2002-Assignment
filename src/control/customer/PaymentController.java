f23c34fe2767f222a3192fc5bd5836fae5bef25f
SQ-T30 <128040899+SQ-T30@users.noreply.github.com>
2024-04-26 23:47:46 +0800

    Update PaymentController.java

--- src/control/customer/PaymentController.java
+++ src/control/customer/PaymentController.java
@@ -1,6 +1,13 @@
 package control.customer;
 
 public class PaymentController{
+
+	/**
+     * processPayment() check if payment provide by customer is sufficient
+     * @param total The total amount to pay
+	* @param total The amount to given by customer
+     * @return boolean
+     */
 	
 	public boolean processPayment(double total, double payment) {
 		if (payment>=total)	return true;
