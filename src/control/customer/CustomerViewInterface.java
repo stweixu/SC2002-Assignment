2206ff4de67b8e5b90acd7feb585bc72790c95d1
SQ-T30 <128040899+SQ-T30@users.noreply.github.com>
2024-04-26 23:55:21 +0800

    Update CustomerViewInterface.java

--- src/control/customer/CustomerViewInterface.java
+++ src/control/customer/CustomerViewInterface.java
@@ -1,6 +1,11 @@
 package control.customer;
 
 public interface CustomerViewInterface {
+
+	/**
+     * Interface for customer to view order details
+     */
+	
 	public void viewOrderStatus(String branchName, String orderID);
 	public double viewOrderDetails(String branchName, String orderID);
 }
