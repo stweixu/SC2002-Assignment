71638ae907def571f775f1965125a88aeb230ba7
SQ-T30 <128040899+SQ-T30@users.noreply.github.com>
2024-04-26 23:15:40 +0800

    Update ExcelRW.java

--- src/control/ExcelRW.java
+++ src/control/ExcelRW.java
@@ -11,19 +11,22 @@ import java.util.ArrayList;
 public class ExcelRW {
 	public static ArrayList<Object[]> readFile(String pathName, int noOfCols) {
 		try {
-			FileInputStream file = new FileInputStream(pathName);
-	        Workbook workbook = new XSSFWorkbook(file);
-	        Sheet sheet = workbook.getSheetAt(0);
-	        
-	        ArrayList<Object[]> entry = new ArrayList<Object[]>();
-	        
-	        for (Row row : sheet) {
-	            if (isRowEmpty(row))	break;
+			FileInputStream file = new FileInputStream(pathName);	// Create an input stream for the pathName
+	        Workbook workbook = new XSSFWorkbook(file);	// Create a new workbook object based on the input stream
+	        Sheet sheet = workbook.getSheetAt(0);	// Get the first sheet of the excel file. We assume that the excel file read only has 1 sheet
+	       
+		// Creates 2D array (rows dynamic, cols defined by noOfCols) to store excel entries
+	        ArrayList<Object[]> entry = new ArrayList<Object[]>();		// Create placeholder for the rows
 
-	            Object[] colValue = new Object[noOfCols];
-	            
+			// Iterate the rows
+	        for (Row row : sheet) {
+	            if (isRowEmpty(row))	break;	// Exit if no more rows to read
+
+	            Object[] colValue = new Object[noOfCols];	// Create placeholder for columns
+
+			// Iterate the columns
 	            for (int i=0; i<noOfCols; i++) {
-	            	Cell cell = row.getCell(i);
+	            	Cell cell = row.getCell(i);	
 	                switch (cell.getCellType()) {
 	                    case STRING:
 	                    	colValue[i] = cell.getStringCellValue();
@@ -35,10 +38,11 @@ public class ExcelRW {
 	                    	colValue[i] = "-";
 	                }
 	            }
-	            entry.add(colValue);
+	            entry.add(colValue);	// update cells read to the placeholder
 	        }
-	        
-	        workbook.close();
+
+			// Close the workbook and input stream when done
+	        workbook.close();		
             file.close();
             
             return entry;
@@ -50,10 +54,10 @@ public class ExcelRW {
 	
 	public static boolean writeFile(ArrayList<Object[]> table, String pathName, int noOfCol) {		
 		try {
-			FileInputStream fin = new FileInputStream(pathName);
-			Workbook workbook = WorkbookFactory.create(fin);		
-			Sheet sheet = workbook.createSheet();
-			workbook.removeSheetAt(0);
+			FileInputStream fin = new FileInputStream(pathName);	// Create an input stream for the pathName
+			Workbook workbook = WorkbookFactory.create(fin);		// Create a new workbook object based on the input stream
+			Sheet sheet = workbook.createSheet();		// Create new sheet for the excel file
+			workbook.removeSheetAt(0);			// Remove existing sheet
 			
 			int size = table.size();
 			
@@ -69,12 +73,15 @@ public class ExcelRW {
 	    		}
 	    	}
             
-            fin.close();
-           
-            FileOutputStream fout = new FileOutputStream(pathName);
+            fin.close();	// Close input stream when done
+
+		// Create output stream and write table entries to excel file
+            FileOutputStream fout = new FileOutputStream(pathName);	
             workbook.write(fout);
+
+		// Close output stream and workbook when done
             fout.close();
-			workbook.close();
+		workbook.close();
             
             System.out.println("Successfully written to file!");
             return true;
@@ -83,7 +90,8 @@ public class ExcelRW {
 	    	return false;
 	    }
 	}
-	
+
+	// Checks whether a row in the excel sheet is empty. Used to stop reads when no more rows to read
 	public static boolean isRowEmpty(Row row) {
         if (row == null || row.getLastCellNum() <= 0) {
             return true;
