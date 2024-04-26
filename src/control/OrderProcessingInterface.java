46ddf4b0a525d2c56be3918ac7d7dbad0ad740cd
uranuswolf <158137715+uranuswolf@users.noreply.github.com>
2024-04-26 22:59:19 +0800

    Update OrderProcessingInterface.java

--- src/control/OrderProcessingInterface.java
+++ src/control/OrderProcessingInterface.java
@@ -4,8 +4,8 @@ public interface OrderProcessingInterface {
      /**
      * An interface for Staff to update the status of the 
        processed order from a new order to be “Ready to pickup"
-     * @param branchName, a specific branch in String.
-     * @param id , id of an order in String.
+     * @param branchName The specific branch where the order came from in String.
+     * @param id The specific order's ID in String.
      * @return void.
      */
 	public void processOrder(String branchName, String id);
