8b91e648a11bedf7be0f7bd78c7841dad7e472b3
SQ-T30 <128040899+SQ-T30@users.noreply.github.com>
2024-04-26 23:41:47 +0800

    Update PasswordController.java

--- src/control/PasswordController.java
+++ src/control/PasswordController.java
@@ -4,6 +4,13 @@ import entity.User;
 import exception.WrongPasswordException;
 
 public class PasswordController {
+    /**
+     * changePassword() is to print out the menu items in a specific branch
+     * @param user The user to check against the database.
+     * @param oldPassword The old password to check against the database in String.
+     * @param newPassword The new password to set for the user in String.
+     * @return void.
+     */
 
     public static boolean changePassword(User user, String oldPassword, String newPassword) throws WrongPasswordException{
         if (AccountController.checkValid(user, oldPassword)){    // Check if user exists and if old password matches    
